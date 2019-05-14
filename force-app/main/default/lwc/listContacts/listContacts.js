e39679c353a1e6995c1f411d62fc3ab13df10ed4
mlezer-bd <martin.lezer@businessdecision.com>
2019-05-14 15:33:35 +0200

    Initial commi'

---
+++ force-app/main/default/lwc/listContacts/listContacts.js
@@ -0,0 +1,58 @@
+import { LightningElement, track, wire } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ListContactsController.getContacts';
+import { refreshApex } from '@salesforce/apex';
+
+export default class ListContacts extends LightningElement {
+    @wire(getContacts) contacts;
+
+    @track contactId;
+    firstName = '';
+    lastName = '';
+    email = '';
+
+    handleFirstNameChange(event) {
+        this.firstName = event.target.value;
+    }
+    handleLastNameChange(event) {
+        this.lastName = event.target.value;
+    }
+    handleEmailChange(event) {
+        this.email = event.target.value;
+    }
+
+    createContact(){
+        const fields = {};
+        fields[FIRSTNAME_FIELD.fieldApiName] = this.firstName;
+        fields[LASTNAME_FIELD.fieldApiName] = this.lastName;
+        fields[EMAIL_FIELD.fieldApiName] = this.email;
+        console.log(fields);
+        const recordInput = { apiName: CONTACT_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then(contact => {
+                this.contactId = contact.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Contact created',
+                        variant: 'success',
+                    }),
+                );
+                refreshApex(this.contacts);
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+    }
+}
